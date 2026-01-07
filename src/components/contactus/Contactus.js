96245942feea65766e8a5189ace6957d3b97883a
JAIGANESH K <karthikeyanjaiganesh555@gmail.com>
2026-01-07 20:08:18 +0530

    Update API URL for production backend

--- src/components/contactus/Contactus.js
+++ src/components/contactus/Contactus.js
@@ -24,7 +24,7 @@ export default function Contactus() {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/submitForm",
+        "https://jaiganesh.pythonanywhere.com/api/submitForm/",
         formData
       );
       console.log(response.data.message); // Log the response from the backend
